9f48e7d1b4d812cd2537c26e32f81fe745b75c86
Vladyslav Vdovychenko <vlad182ava@gmail.com>
2023-01-16 19:30:21 +0200

    radio buttons

--- ScanAR macOS/Content/Home/View/HomeViewController.swift
+++ ScanAR macOS/Content/Home/View/HomeViewController.swift
@@ -12,19 +12,46 @@ final class HomeViewController: NSViewController {
     
     var output: HomeViewOutput!
 
-    @IBOutlet weak var cell: NSTextFieldCell!
     @IBOutlet weak var label: NSTextField!
+    
+    @IBOutlet weak var previewRadioButton: NSButton!
+    @IBOutlet weak var lowRadioButton: NSButton!
+    @IBOutlet weak var mediumRadioButton: NSButton!
+    @IBOutlet weak var highRadioButton: NSButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.label.stringValue = "Hello, 123"
-        }
     }
-
+    
 }
 
 extension HomeViewController: HomeViewInput {
 
+}
+
+private extension HomeViewController {
+    
+    @IBAction func didChangeRadioButtonValue(_ sender: NSButton) {
+        switch sender {
+        case previewRadioButton:
+            printRadioButton("preview")
+            
+        case lowRadioButton:
+            printRadioButton("low")
+            
+        case mediumRadioButton:
+            printRadioButton("medium")
+            
+        case highRadioButton:
+            printRadioButton("high")
+            
+        default:
+            break
+        }
+    }
+    
+    func printRadioButton(_ text: String) {
+        self.label.stringValue = text
+    }
+    
 }
